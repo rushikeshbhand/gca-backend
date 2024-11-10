77cc8cbee6a1aca16711c084861a9d891134cdb1
rushikeshbhand <rushikeshgbhand@gmail.com>
2024-11-10 19:13:09 +0530

    make changes in vercel configuration

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const contactRoutes = require('./routes/contactRoutes');
-
+const app = express();
 dotenv.config();
 
-const app = express();
-app.use(bodyParser.json());
 app.use(cors({
   origin: '*' // Allows all origins
 }));
 
+
+app.use(bodyParser.json());
+
 // Contact form route
 app.use('/api/contact', contactRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
